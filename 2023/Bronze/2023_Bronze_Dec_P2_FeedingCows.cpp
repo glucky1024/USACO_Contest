9492c2fe53bfe0ff8548b40cc5c06e1d45be6e26
chicken <cc@welsim.com>
2023-12-16 11:25:56 -0500

    2023 dec bronze p2 and p3 files

--- 2023/Bronze/2023_Bronze_Dec_P2_FeedingCows.cpp
+++ 2023/Bronze/2023_Bronze_Dec_P2_FeedingCows.cpp
@@ -1,5 +1,5 @@
 /*
-PROG: cow college
+PROG: feeding the cows
 LANG: C++11
 */
 
@@ -17,7 +17,20 @@ LANG: C++11
 
 using namespace std;
 
-bool ReadInFile(const string& sFile, vector<long>& cowMoney)
+class CTest
+{
+public:
+    int _nCows; // 5
+    int _nMoves; // 0
+    string _sTypes; // GHHGG
+
+public:
+    CTest(int c, int m, const string& s)
+        : _nCows(c), _nMoves(m), _sTypes(s) {
+    }
+};
+
+bool ReadInFile(const string& sFile, vector<CTest*>& tests)
 {
     fstream in_file;
     in_file.open(sFile, ios::in);
@@ -27,14 +40,17 @@ bool ReadInFile(const string& sFile, vector<long>& cowMoney)
         return false;
     }
 
-    long nCows = 0;
-    in_file >> nCows;
-    cowMoney.clear();
-    for (int i = 0; i < nCows; i++)
+    int nTests = 0;
+    in_file >> nTests;
+    for (int i = 0; i < nTests; ++i)
     {
-        long m = 0;
-        in_file >> m;
-        cowMoney.push_back(m);
+        int nCows = 0, nMoves = 0;
+        in_file >> nCows >> nMoves;
+        string s;
+        in_file >> s;
+
+        CTest* pTest = new CTest(nCows, nMoves, s);
+        tests.push_back(pTest);
     }
 
     return true;
@@ -57,12 +73,14 @@ bool ReadInCout(vector<long>& cowMoney)
     return true;
 }
 
-void Print(const int& nCows, const vector<int>& cowMoney)
+void Print(const vector<CTest*>& tests)
 {
-    cout << nCows << endl;
-    for (int i = 0; i < nCows; i++)
+    cout << tests.size() << endl;
+    for (int i = 0; i < tests.size(); i++)
     {
-        cout << cowMoney[i] << " ";
+        CTest* pT = tests[i];
+        cout << pT->_nCows << " " << pT->_nMoves << endl;
+        cout << pT->_sTypes << endl;
     }
     cout << endl;
 }
@@ -78,17 +96,119 @@ void WriteOut(const string& sFile, int a, int d)
     //out_file.close();
 }
 
-int main()
+void addPatch()
+{
+
+}
+
+int main2()
 {
     // Step1: read
-    vector<long> cowMoney;
-    //bool bOk = ReadInFile("prob1_bronze_dec22/1.in", cowMoney);
-    bool bOk = ReadInCout(cowMoney);
+    vector<CTest*> tests;
+
+    bool bOk = ReadInFile("prob2_bronze_dec22/1.in", tests);
+    //bool bOk = ReadInCout(cowMoney);
     if (!bOk) { return 0; }
-    //Print(nCows, cowMoney);
+    //Print(tests);
 
+    for (int i = 0; i < tests.size(); i++)
+    {
+        CTest* pT = tests[i];
+        int nSlots = pT->_sTypes.size();
+        int count = 0;
+        string sGP;
+        for (int j = 0; j < nSlots; ++j)
+            sGP += ".";
+        for (int j = 0; j < nSlots; ++j)
+        {
+            bool bFound = false;
+            string sCow = pT->_sTypes.substr(j, 1);
+            //for (int k = max(0, j - pT->_nMoves); k <= min(nSlots - 1, j + pT->_nMoves); ++k) {
+            //    if (sCow == sGP.substr(k, 1)) {
+            //        bFound = true;
+            //        break;
+            //    }
+            //}
+            for (int k = j + pT->_nMoves; k > j - pT->_nMoves; --k) {
+                if (k >= nSlots || k < 0)
+                    continue;
+                if (sCow == sGP.substr(k, 1)) {
+                    bFound = true;
+                    break;
+                }
+            }
 
-    WriteOut("collegeCows.out", amount, dollar);
+            if (!bFound) {
+                int pos = min(nSlots - 1- pT->_nMoves, j + pT->_nMoves);
+                while (sGP.substr(pos, 1) != ".") {
+                    pos--;
+                }
+
+                sGP.replace(pos, 1, sCow);
+                count++;
+            }
+        }
+
+        cout << count << endl;
+        cout << sGP << endl;
+    }
+    
+    //WriteOut("collegeCows.out", count, );
+
+    for (CTest* t : tests)
+        delete t;
+    tests.clear();
 
     return 0;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        int k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        int patchG = -k - 1; // first patch location which does not cover cow 1
+        int patchH = -k - 1; // first patch location which does not cover cow 1
+
+        int cnt = 0;
+        string ans(n, '.');
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'G' && i - patchG > k) {
+                // the nearest G patch we placed does not cover cow i
+                ++cnt;
+                if (i + k >= n) {
+                    patchG = i;
+                    if (ans[patchG] == 'H') { patchG--; }
+                }
+                else {
+                    patchG = i + k; // place the G patch k away
+                }
+                ans[patchG] = 'G';
+            }
+            if (s[i] == 'H' && i - patchH > k) {
+                // the nearest H patch we placed does not cover cow i
+                ++cnt;
+                if (i + k >= n) {
+                    patchH = i;
+                    if (ans[patchH] == 'G') { patchH--; }
+                }
+                else {
+                    patchH = i + k; // place the H patch k away
+                }
+                ans[patchH] = 'H';
+            }
+        }
+        cout << cnt << endl << ans << endl;
+    }
 }
--- 2023/Bronze/2023_Bronze_Dec_P3_ReverseEngineering.cpp
+++ 2023/Bronze/2023_Bronze_Dec_P3_ReverseEngineering.cpp
@@ -1,20 +1,134 @@
-// 2023_Bronze_Dec_P3_ReverseEngineering.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+PROG: feeding the cows
+LANG: C++11
+*/
 
 #include <iostream>
+#include <fstream>
+#include <cassert>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+#include <climits>
+
+
+using namespace std;
+
+class CTest
+{
+public:
+    int _nLength; //1 
+    int _nLines;//3
+    vector<string> _vecInput; //0, 0, 1
+    vector<int> _vecOutput; //0, 0, 1
+
+public:
+    CTest(int len, int line, vector<string> in, vector<int> out)
+        : _nLength(len), _nLines(line), _vecInput(in), _vecOutput(out) {
+    }
+};
+
+bool ReadInFile(const string& sFile, vector<CTest*>& tests)
+{
+    fstream in_file;
+    in_file.open(sFile, ios::in);
+    if (!in_file.is_open())
+    {
+        cout << "Error: cannot open input file." << endl;
+        return false;
+    }
+
+    int nTests = 0;
+    in_file >> nTests;
+    for (int i = 0; i < nTests; ++i)
+    {
+        int nLength = 0, nLines = 0;
+        in_file >> nLength >> nLines;
+        vector <string> in;
+        vector <int> out;
+
+        for (int j = 0; j < nLines; j++)
+        {
+            string s;
+            int o=0;
+            in_file >> s >> o;
+            in.push_back(s);
+            out.push_back(o);
+        }
+
+        CTest* pTest = new CTest(nLength, nLines, in, out);
+        tests.push_back(pTest);
+    }
+
+    return true;
+}
+
+//bool ReadInCout(vector<long>& cowMoney)
+//{
+//    ios::sync_with_stdio(0); cin.tie(0);
+//
+//    int nCows = 0;
+//    cin >> nCows;
+//    cowMoney.clear();
+//    for (int i = 0; i < nCows; i++)
+//    {
+//        long m = 0;
+//        cin >> m;
+//        cowMoney.push_back(m);
+//    }
+//
+//    return true;
+//}
+
+void Print(const vector<CTest*>& tests)
+{
+    cout << tests.size() << endl << endl;
+    for (int i = 0; i < tests.size(); i++)
+    {
+        CTest* pt = tests[i];
+        cout << pt->_nLength << " " << pt->_nLines << endl;
+        for (int i = 0; i < pt->_nLines; i++)
+        {
+            cout << pt->_vecInput[i] <<" " << pt->_vecOutput[i] << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void WriteOut(const string& sFile, int a, int d)
+{
+    cout << a << " " << d << endl;
+
+    //ofstream out_file;
+    //out_file.open(sFile);
+    //out_file << a << " " << d << endl;
+
+    //out_file.close();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Step1: read
+    vector<CTest*> tests;
+
+    bool bOk = ReadInFile("prob3_bronze_dec22/1.in", tests);
+    //bool bOk = ReadInCout(cowMoney);
+    if (!bOk) { return 0; }
+    Print(tests);
+
+
+
+
+
+    //WriteOut("collegeCows.out", minPatch);
+
+    for (CTest* t : tests)
+        delete t;
+    tests.clear();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
