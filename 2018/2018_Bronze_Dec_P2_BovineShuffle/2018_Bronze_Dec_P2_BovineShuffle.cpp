1d312124cc6558dd0f7d06520b4721da5be46f8b
chicken <cc@welsim.com>
2024-01-13 16:15:13 -0500

    BRUH this question was so confusing to understanddd

--- 2018/2018_Bronze_Dec_P2_BovineShuffle/2018_Bronze_Dec_P2_BovineShuffle.cpp
+++ 2018/2018_Bronze_Dec_P2_BovineShuffle/2018_Bronze_Dec_P2_BovineShuffle.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-bool ReadInFile(const string& sFile, vector<int>& bills, vector<int>& trucks)
+bool ReadInFile(const string& sFile, vector<int>& movesTo, vector<string>& ids)
 {
     fstream in;
     in.open(sFile, ios::in);
@@ -21,25 +21,64 @@ bool ReadInFile(const string& sFile, vector<int>& bills, vector<int>& trucks)
         return false;
     }
 
+    int n;
+    movesTo.clear();
+    ids.clear();
 
+    in >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        in >> temp;
+        movesTo.push_back(temp);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        string temp;
+        in >> temp;
+        ids.push_back(temp);
+    }
 
     return true;
 }
 
-void Print(const vector<int> bills, const vector<int> trucks)
+void Print(const vector<int> movesTo, vector<string> ids)
 {
- 
+    cout << movesTo.size() << endl;
+
+    for (int i = 0; i < movesTo.size(); i++)
+    {
+        cout << movesTo[i] << " ";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < movesTo.size(); i++)
+    {
+        cout << ids[i] << " ";
+    }
+
+    cout << endl;
 }
 
-bool WriteOut(const string& sFile, const int ans)
+bool WriteOut(const string& sFile, const vector<string> ans)
 {
-    cout << ans;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << endl;
+    }
 
     //
     ofstream out_file;
     out_file.open(sFile);
 
-    out_file << ans << endl;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        out_file << ans[i] << endl;
+    }
+
 
     out_file.close();
 
@@ -48,9 +87,23 @@ bool WriteOut(const string& sFile, const int ans)
 
 int main()
 {
+    vector<int> movesTo;
+    vector<string> ids;
 
-    bool bOk = ReadInFile("../billboard_bronze_dec17/1.in", bills, trucks);
-    //Print(bills, trucks);
+    bool bOk = ReadInFile("shuffle.in", movesTo, ids);
+    Print(movesTo, ids);
 
-    WriteOut("backforth.out", ans);
+    int nCows = movesTo.size();
+
+    for (int k = 0; k < 3; k++)
+    {
+        std::vector<std::string> old_pos(nCows);
+        for (int i = 0; i < nCows; i++)
+        {
+            old_pos[i] = ids[movesTo[i] - 1];
+        }
+        ids = old_pos;
+    }
+
+    WriteOut("shuffle.out", ids);
 }
