8c860f0fc8daa2756342e95d06a159c7a0542d92
chicken <cc@welsim.com>
2024-01-15 10:39:11 -0500

    bruh I made this solution way more convoluted than it should've been, but the solution that's on usaco was wrong for me too?? atleast I got all 10 testcases eventually.

--- 2017/2017_Bronze_Dec_P1_SquarePasture/2017_Bronze_Dec_P1_SquarePasture.cpp
+++ 2017/2017_Bronze_Dec_P1_SquarePasture/2017_Bronze_Dec_P1_SquarePasture.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 
-bool ReadInFile(const string& sFile, vector<pair<int, int>>& nameChange)
+bool ReadInFile(const string& sFile, vector<int>& rect1, vector<int>& rect2)
 {
     fstream in;
     in.open(sFile, ios::in);
@@ -22,12 +22,26 @@ bool ReadInFile(const string& sFile, vector<pair<int, int>>& nameChange)
         return false;
     }
 
+    for (int i = 0; i < 4; i++)
+    {
+        int temp1;
+        in >> temp1;
 
+        rect1.push_back(temp1);
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        int temp1;
+        in >> temp1;
+
+        rect2.push_back(temp1);
+    }
 
     return true;
 }
 
-void Print(const vector<pair<int, int>> nameChange)
+void Print(const vector<int> rect1, const vector<int> rect2)
 {
 }
 
@@ -52,12 +66,80 @@ bool WriteOut(const string& sFile, const int ans)
 
 int main()
 {
-    vector<pair<int, int>> nameChange;
-    int prevLead = 5;
+    vector<int> rect1;
+    vector<int> rect2;
 
-    bool bOk = ReadInFile("../measurement_bronze_dec17/1.in", nameChange);
-    //Print(nameChange);
+    bool bOk = ReadInFile("square.in", rect1, rect2);
+    //Print(rect1, rect2);
 
+    int rect1L = rect1[2] - rect1[0];
+    int rect2L = rect2[2] - rect2[0];
 
-    WriteOut("measurement.out", changes);
+    int length = 0, height = 0;
+
+    int miniX = 10, maxiX = 0, miniY = 10, maxiY = 0;
+
+    if (rect1[0] >= rect2[0] && rect1[2] <= rect2[2])
+    {
+        length = rect2L;
+    }
+
+    else if (rect1[0] <= rect2[0] && rect1[2] >= rect2[2])
+    {
+        length = rect1L;
+    }
+
+    else if (rect1[2] > rect2[2] && rect1[0] > rect2[0])
+    {
+        //rectangle 1 is longer than 2
+        length = rect2L + (rect1[2] - rect2[2]);
+    }
+
+    else if (rect1[2] < rect2[2] && rect1[0] < rect2[0])
+    {
+        //rectangle 2 is longer than 1
+        length = rect1L + (rect2[2] - rect1[2]);
+    }
+    else
+    {
+        int maxiL = max(rect1[1], rect2[1]);
+        int miniL = min(rect1[3], rect2[3]);
+        length = abs(maxiL - miniL) + rect1L + rect2L;
+    }
+
+    int rect1H = rect1[3] - rect1[1];
+    int rect2H = rect2[3] - rect2[1];
+
+    if (rect1[1] >= rect2[1] && rect1[3] <= rect2[3])
+    {
+        height = rect2H;
+    }
+
+    else if (rect1[1] <= rect2[1] && rect1[3] >= rect2[3])
+    {
+        height = rect1H;
+    }
+
+    else if (rect1[3] > rect2[3] && rect1[1] > rect2[1])
+    {
+        //rectangle 1 is higher than 2
+        height = rect2H + (rect1[3] - rect2[3]);
+    }
+
+    else if (rect1[3] < rect2[3] && rect1[1] < rect2[1])
+    {
+        //rectangle 2 is higher than 1
+        height = rect1H + (rect2[3] - rect1[3]);
+    }
+    else
+    {
+        int maxiH = max(rect1[1], rect2[1]);
+        int miniH = min(rect1[3], rect2[3]);
+        height = abs(maxiH - miniH) + rect1H + rect2H;
+    }
+
+    int maxFR = max(length, height);
+    int area = maxFR * maxFR;
+
+    WriteOut("square.out", area);
 }
